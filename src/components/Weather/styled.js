e37d69abf9858acf1b6cd50297bc91d4acb2d4fa
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2024-01-15 22:18:55 +0100

    added google font, changed some styles

--- src/components/Weather/styled.js
+++ src/components/Weather/styled.js
@@ -1,22 +1,23 @@
 import styled from "styled-components";
 
-
-
+export const StyledSection = styled.section`
+    margin: 10px 0;
+    background-color: ${({ theme }) => theme.colors.white};
+    box-shadow: 0 0 5px #ddd;
+`;
 export const Wrapper = styled.div`
    display: flex;
    flex-direction: column;
-   height: 100vh;
    justify-content: center;
-   max-width: 1000px;
    background-color: ${({ theme }) => theme.colors.white};
-   margin: 0 auto;
-
+   border-radius: 5px;
+   box-shadow:  -2px 6px 18px 0.5px rgb(152 158 185);
 `;
 
 export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 30px;
+   display: flex;
+   flex-direction: column;
+   gap: 30px;
    padding: 20px;
    color: green;
    margin: 20px;
@@ -25,16 +26,41 @@ export const Form = styled.form`
 export const Input = styled.input`
    width: 400px;
    padding: 20px;
+   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px){
+      max-width: 200px;
+      padding: 10px;
+   }
 `;
 
 export const Button = styled.button`
    width: 400px;
    padding: 20px;
+   font-size: 16px;
    background-color: ${({ theme }) => theme.colors.teal};
+   cursor: pointer;
+   border: none;
+   transition: 0.3s;
+   color:${({ theme }) => theme.colors.white};
+   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px){
+      max-width: 200px;
+      padding: 10px;
+      font-size: 12px;
+   }
+   &:hover{
+   scale: 1.1;
+  
+   }
 `;
 
 export const Paragraph = styled.p`
-   font-size: 14px;
+   font-size: 18px;
    color: black;
    margin-left: 30px;
+`;
+export const Title = styled.h2`
+    font-size: 26px;
+    margin-left: 12px;
+    font-weight: 600px;
+    color: ${({ theme }) => theme.colors.teal};
+   
 `;
--- src/globalStyle.js
+++ src/globalStyle.js
@@ -15,9 +15,13 @@ html {
 
 body {
   font-family: 'Montserrat', sans-serif;
+  font-family: 'Roboto Mono', monospace;
   background-image: url(${background});
   background-size: cover;
   position: relative;
+  height: 100vh;
   background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
 }
 `;
